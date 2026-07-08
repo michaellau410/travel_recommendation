e2571ed24985a62906799c4c97cfe7bb816ea152
x <x@gmail.com>
2026-07-08 13:29:53 -0400

    edit

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,5 +1,19 @@
 const btnSearch = document.getElementById('btnSearch');
 
+function handleInput(userInput)
+{
+    if(userInput.toLowerCase() === "country")
+        return "countries";
+
+    if(userInput.toLowerCase() === "temple")
+        return "temples";
+
+    if(userInput.toLowerCase() === "beach")
+        return "beaches";
+
+    return userInput.toLowerCase();
+}
+
 function searchDest()
 {
 	const input = document.getElementById('searchInput').value.toLowerCase();
@@ -11,24 +25,29 @@ function searchDest()
 	  .then(response => response.json())
 	  .then(data => 
         {
-            console.log(Object.keys(data));
-
-		    const countries = data.countries.filter(item => item.name.toLowerCase() === input);
-			if (countries)
+		    const key = Object.keys(data).find(item => item.toLowerCase() === handleInput(input));
+			if (key)
             {
-                console.log(countries.name);
-                /*
-			  const symptoms = condition.symptoms.join(', ');
-			  const prevention = condition.prevention.join(', ');
-			  const treatment = condition.treatment;
+                const selectedArray = data[key];
+                for(const item of selectedArray)
+                {
+                    console.log(item.name);
 
-			  resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-			  resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+                    let appended = `<div class="city-card">`
+                    + `<div class="city-photo-wrapper">`
+                    + `<img src="${item.imageUrl}" alt="${item.name}" class="city-photo">`
+                    + `</div>`
+                    + `<div class="city-info">`
+                    + `<h3>${item.name}</h3>`
+                    + `<p class="city-desc">${item.description}</p>`
+                    + `</div>`
+                    + `</div>`;
 
-			  resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-			  resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-			  resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
-                */
+                    destinations.innerHTML += appended;
+                    
+                }
+
+
             } else {
                 destinations.innerHTML = 'No Result.';
 			}
